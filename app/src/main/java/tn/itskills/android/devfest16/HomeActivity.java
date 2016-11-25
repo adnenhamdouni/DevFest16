9356c65123b57c2886024e5eb984cbbb2b283a59
Adnen HAMDOUNI <adnen.hamdouni@squeezer-software.com>
2016-11-25 10:44:05 +0100

    Add post message, sage message into database

--- app/src/main/java/tn/itskills/android/devfest16/HomeActivity.java
+++ app/src/main/java/tn/itskills/android/devfest16/HomeActivity.java
@@ -3,7 +3,6 @@ package tn.itskills.android.devfest16;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -26,8 +25,8 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(HomeActivity.this, NewPostActivity.class));
+
             }
         });
 
